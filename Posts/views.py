230dfc1e0a992c5f6d48320a58ad06e67e8f6e1b
MaxAlekseevDev <dev.max.alekseeev@gmail.com>
2020-02-14 23:52:58 +0200

    create base.html & update templates

--- Posts/views.py
+++ Posts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello(request):
-    return render(request, 'blog/tech-index.html')
+    return render(request, 'blog/index.html')
